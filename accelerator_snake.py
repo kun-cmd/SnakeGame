82cc6fbb2cd1ec6984afa59c72048c4d30bcb903
kun-cmd <128396716+kun-cmd@users.noreply.github.com>
2024-04-05 16:10:52 +0800

    Update accelerator_snake.py

--- accelerator_snake.py
+++ accelerator_snake.py
@@ -1,14 +1,6 @@
-import pygame
-import pygame_gui
-import sys
-import random
-
 from snake王传恩 import *
 from decimal import Decimal
-from pygame_gui.core import ObjectID
-#添加音频
-from pygame.locals import *
-from pygame import mixer
+import pygame
 # 定义颜色
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -17,13 +9,16 @@ BLUE = (0, 0, 255)
 GOLD = (240,230,140)
 
 class Acc_snake(Snake):
-    def __init__(self, positions, color, is_ikun=False):
+    def __init__(self, positions, color, is_ikun=False,normal_speed=100):
         super().__init__(positions, color, is_ikun)
+        global block_size,block_size_decimal
         self.max_stamina = 100.0
         self.current_stamina = 100.0
         self.stam_recharge_tick = 0.05
         self.stam_recharge_acc = 0.0
-        self.rect = pygame.Rect((Snake.get_head_position()),(block_size))
+        self.acc = False
+        self.normal_speed = normal_speed
+        self.rect = pygame.Rect(self.get_head_position(),(block_size,block_size))
     def move(self):
         global block_size,block_size_decimal
         cur = self.get_head_position()
@@ -33,6 +28,7 @@ class Acc_snake(Snake):
         x = float(Decimal(str(cur[0])) +(x*block_size_decimal))
         y = float(Decimal(str(cur[1])) +(y*block_size_decimal))
         new = (x, y)
+        # if self.acc:
         self.current_stamina -= 0.4
         self.current_stamina = max(self.current_stamina, 0)
         
@@ -63,12 +59,15 @@ class Acc_snake(Snake):
             if self.stam_recharge_acc >= self.stam_recharge_tick:
                 self.current_stamina += 1
                 self.stam_recharge_acc = 0.0
+        min(self.current_stamina,self.max_stamina)
     def get_stamina_percentage(self) -> float:
         return self.current_stamina/self.max_stamina
 
-    def skill(self):
-        keys = pygame.key.get_pressed()
-
-        if keys[pygame.K_SPACE]:
+    def skill(self,is_pressed):
+        if is_pressed:
+            self.acc = True
             self.speed = self.speed -30
-            
+            print(self.speed)
+        else:
+            self.speed = self.normal_speed
+            self.acc = False
