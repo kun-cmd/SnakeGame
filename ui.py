ab41a0183da755c437e5625c48101b1e8e1840c7
kun-cmd <128396716+kun-cmd@users.noreply.github.com>
2024-04-05 15:15:08 +0800

    Create ui.py

---
+++ ui.py
@@ -0,0 +1,23 @@
+import random
+import pygame
+import pygame_gui
+from collections import deque
+
+from pygame_gui import UIManager, PackageResource
+
+from pygame_gui.elements import UIWindow
+from pygame_gui.elements import UIButton
+from pygame_gui.elements import UIHorizontalSlider
+from pygame_gui.elements import UITextEntryLine
+from pygame_gui.elements import UIDropDownMenu
+from pygame_gui.elements import UIScreenSpaceHealthBar
+from pygame_gui.elements import UILabel
+from pygame_gui.elements import UIImage
+from pygame_gui.elements import UIPanel
+from pygame_gui.elements import UISelectionList
+
+from pygame_gui.windows import UIMessageWindow
+
+class UI:
+    def __init__(self) -> None:
+        pass
