34bf3a843f058fce58dc0fab7090aabbb64b581e
kun-cmd <128396716+kun-cmd@users.noreply.github.com>
2024-04-08 22:02:48 +0800

    Update b.py

--- b.py
+++ b.py
@@ -1,2 +1,12 @@
-import pygame
-pygame.init()
+import math
+import numpy as np
+positions = []
+center_x = 0 
+center_y = 0
+radius = 1
+
+for angle in np.linspace(0, 2*math.pi, 100):
+  x = center_x + radius * math.cos(angle)
+  y = center_y + radius * math.sin(angle)  
+  positions.append((x, y))
+  print(positions)
