5f860b392820635fe5445ec3a3987e9935b03ef2
kun-cmd <128396716+kun-cmd@users.noreply.github.com>
2024-04-08 22:02:32 +0800

    Update setting.py

--- setting.py
+++ setting.py
@@ -5,7 +5,9 @@ from pygame import mixer
 import random
 import sys
 from pygame_gui.core import ObjectID
+from pygame_gui import elements
 from decimal import Decimal
+import math
 mixer.init()
 # 定义颜色
 WHITE = (255, 255, 255)
@@ -26,6 +28,8 @@ SHIELD_BROKEN = pygame.USEREVENT+4
 ACC_EVENT1 =  pygame.USEREVENT+5
 ACC_EVENT2 =  pygame.USEREVENT+6
 MOVE_EVENT =  pygame.USEREVENT+7
+SLOW_EVENT1 =  pygame.USEREVENT+8
+SLOW_EVENT2 =  pygame.USEREVENT+9
 #帧率
 FPS = 60
 width, height = 800, 600
